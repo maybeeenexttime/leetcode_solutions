efadf1ccd67f10866cb8d2897eda462e9011f88d
Bekzhan <bekjando@gmail.com>
2022-06-10 22:14:49 +0300

    new 5 ex. (my version)

--- src/leetcode_5/Solution.java
+++ src/leetcode_5/Solution.java
@@ -2,26 +2,29 @@ package leetcode_5;
 
 
 public class Solution {
-    public int maxLen = 0;
-    public int start = 0;
+    int resultStart;
+    int resultLength;
 
 
     public String longestPalindrome(String s) {
-            for(int i = 0; i < s.length(); i++) {
-                expand(i,i + 1, s);
-                expand(i, i, s);
-            }
-            return s.substring(start, start + maxLen);
+           int strLength = s.length();
+           if(strLength < 2) return s;
+        for (int i = 0; i < strLength; i++) {
+            expand(s, i, i+1);
+            expand(s, i, i);
+        }
+
+            return s.substring(resultStart, resultStart + resultLength);
     }
 
-    private void expand(int left, int right, String s) {
+    private void expand(String s, int left, int right) {
             while (left >= 0 && right < s.length() && s.charAt(left)==s.charAt(right)) {
                 left--;
                 right++;
             }
-            if(maxLen < right - left + 1) {
-                maxLen = right - left - 1;
-                start = left + 1;
+            if(resultLength < right - left - 1) {
+                resultStart = left + 1;
+                resultLength = right - left - 1;
             }
     }
 
