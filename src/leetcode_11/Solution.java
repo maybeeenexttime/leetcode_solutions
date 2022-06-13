01a68cab49234a1955925fc13d58d2c9b7331d29
Bekzhan <bekjando@gmail.com>
2022-06-13 13:27:13 +0300

    new 148 ex. (ver. priority queue)

---
+++ src/leetcode_11/Solution.java
@@ -0,0 +1,28 @@
+package leetcode_11;
+
+public class Solution {
+    public int maxArea(int[] height) {
+        int index = 0;
+        int end = height.length - 1;
+        int areaSum = 0;
+
+        while (index < end){
+           int currentArea = Math.min(height[index],height[end]) * (end - index);
+           if(height[index] < height[end]){
+               index++;
+           }else {
+               end--;
+           }
+            areaSum = Math.max(areaSum, currentArea);
+        }
+
+
+        return areaSum;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        Solution solution = new Solution();
+        System.out.println(solution.maxArea(height));
+    }
+}
