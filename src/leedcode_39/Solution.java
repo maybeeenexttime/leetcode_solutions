8c3ec274136e7881436f29880ae69be0cbddd862
Bekzhan <bekjando@gmail.com>
2022-06-27 20:12:07 +0300

    new 39 ex.

--- src/leedcode_39/Solution.java
+++ src/leedcode_39/Solution.java
@@ -1,20 +1,46 @@
 package leedcode_39;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-        public List<List<Integer>> combinationSum(int[] candidates, int target) {
-            for (int i = 0; i < candidates.length; i++) {
-                if(candidates[i] + candidates[i+1] == target ){
-                }
-            }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
 
-            return null;
+        int index=0;
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> a = new ArrayList<>();
+        find(index,candidates,target,list,a);
+        return list;
+    }
+
+    public void find(int index,int[] arr,int target,List<List<Integer>> ans,
+                     List<Integer> d){
+
+
+        if(index == arr.length){
+
+            if(target ==0){
+                ans.add(new ArrayList<>(d));
+            }
+            return;
         }
+
+        if(arr[index] <=target){
+            d.add(arr[index]);
+            find(index,arr,target-arr[index],ans,d);
+            d.remove(d.size()-1);
+        }
+        find(index+1,arr,target,ans,d);
+
+
+    }
 
         public static void main(String[] args){
             int[] candidates = {2,3,6,7};
             int target = 7;
+
+            Solution solution =  new Solution();
+            System.out.println(solution.combinationSum(candidates, target));
 
         }
 
